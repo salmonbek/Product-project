7e62c9a96292f5f50bfdac0992cfbb322dbb0b8f
“salmonbek” <salmonlearner34@gmail.com>
2023-12-11 08:51:17 +0500

    try

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const loading = document.getElementById("loading");
+
+const loadingDuration = 1000; // 1s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
 function openNavbar() {
   document.getElementById("navbar-responsive").style.left = "0";
   // document.getElementById("navbar-responsive").style.top = "0";
@@ -9,3 +16,17 @@ function closeNavbar() {
 
 document.getElementById("navbar-open").addEventListener("click", openNavbar);
 document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
